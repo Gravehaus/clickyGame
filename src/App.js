b52f39021b7934fd1638163b96aed8d763c2d774
juliagraves <gravehaus@gmail.com>
2019-10-25 13:51:58 -0700

    attempting to deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import {CARDS} from './cards.json';
+import banner from './components/banner'; ///Game Mat
+import footer from './components/footer'; //Credit
+import navbar from './components/navbar';  ///Keep score in here
+import CARDS from './cards.json';
+
 
 function App() {
 
@@ -22,18 +25,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <code>
+           
+        </code> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     
       </header>
     </div>
   );
---
+++ src/components/footer/footer.js
@@ -0,0 +1,15 @@
+
+import React, {Component} from 'react';
+ 
+export default class Footer extends Component {
+    render(){
+        return (
+             <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Julia Graves 2019</span>
+      </div>
+    </footer>
+ 
+        )
+    }
+}
