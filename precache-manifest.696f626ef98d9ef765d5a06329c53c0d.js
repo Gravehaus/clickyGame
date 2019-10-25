47609c20ed8d455b1fb6f74602c19285dd2e7218
juliagraves <gravehaus@gmail.com>
2019-10-25 14:26:21 -0700

    Updates

---
+++ precache-manifest.696f626ef98d9ef765d5a06329c53c0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5c5faf24840349edc1ca893544b1fd",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e9df732b1ac1478d1081",
+    "url": "/clickyGame/static/css/main.97d12beb.chunk.css"
+  },
+  {
+    "revision": "a04e801e362123d167c9",
+    "url": "/clickyGame/static/js/2.945dc606.chunk.js"
+  },
+  {
+    "revision": "e9df732b1ac1478d1081",
+    "url": "/clickyGame/static/js/main.7429e2ab.chunk.js"
+  },
+  {
+    "revision": "08e2405d66dd095e56b5",
+    "url": "/clickyGame/static/js/runtime-main.250ea920.js"
+  }
+]);
