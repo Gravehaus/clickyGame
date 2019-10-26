af797b17ebff985c7268bb5263322a9f4c93f009
juliagraves <gravehaus@gmail.com>
2019-10-26 13:46:44 -0700

    removed files and replaced.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import header from './components/header';  ///Keep score in here
-import footer from './components/footer'; //Credit
-import CARDS from './cards.json';
+import React, { Component } from "react";
+import Card from "./components/card";
+import Wrapper from "./components/wrapper"; //THIS IS WHERE THE GAME MAT
+import Header from "./components/header";
+import cards from "./cards.json"; ///THIS IS PULLING FROM THE CARDS JSON FILE
+import "./App.css";
+
+///ALL IMPORTS MUST BE IN UPPER CASE.
 
 
-function App() {
+class App extends Component {
+  state = {
+    cards,
+    score: 0,
+    highscore: 0
+  };
 
-  //PUT ALL MY CODE HERE///
-  
-  //PUT ALL MY CODE HERE///
+  gameOver = () => {
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert(`Game Over :( \nscore: ${this.state.score}`);
+    this.setState({score: 0});
+    return true;
+  }
 
-  //PUT ALL MY CODE HERE///
-
-  //PUT ALL MY CODE HERE///
-
-  //PUT ALL MY CODE HERE///
-
-  //PUT ALL MY CODE HERE///
-
-  //PUT ALL MY CODE HERE///
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         <code>
-           
-        </code> 
-        </p>
-     
-      </header>
-    </div>
-  );
+  clickCount = id => {
+    this.state.cards.find((o, i) => {
+      if (o.id === id) {
+        if(cards[i].count === 0){
+          cards[i].count = cards[i].count + 1;
+          this.setState({score : this.state.score + 1}, function(){
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true; 
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score} highscore={this.state.highscore}>SMASH BROS MEMORY GAME</Header>
+        {this.state.cards.map(card => (
+          <Card
+            clickCount={this.clickCount}
+            id={card.id}
+            key={card.id}
+            image={card.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/wrapper/wrapper.js
@@ -0,0 +1,6 @@
+  
+import React from "react";
+
+const wrapper = props => <div className="wrapper">{props.children}</div>;
+
+export default wrapper;
